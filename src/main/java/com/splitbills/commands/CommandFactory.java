01475e5db148d2426a152433af3ea0683f6bb837
Ivelina <ivelinavul@gmail.com>
2020-10-10 11:29:47 +0300

    Fix sintax

--- src/main/java/com/splitbills/commands/CommandFactory.java
+++ src/main/java/com/splitbills/commands/CommandFactory.java
@@ -2,14 +2,20 @@ package com.splitbills.commands;
 
 public class CommandFactory {
 
-    public Command getCommand(String name) throws InvalidArgumentsException {
-        name = name.toLowerCase().trim();
-        CommandName command = CommandName.valueOf(name);
-        return switch (command) {
-            case REGISTER -> new Register();
-            default -> throw new InvalidArgumentsException("There is no command with the given name");
+    public Command getCommand(String inputName) throws InvalidArgumentsException {
+        inputName = inputName.toLowerCase().trim();
+        CommandName commandName = CommandName.valueOf(inputName);
+        Command command;
+        switch (commandName) {
+            case REGISTER:
+                command = new Register();
+                break;
+            default:
+                throw new InvalidArgumentsException("There is no command with the given name");
 
-        };
+        }
+        ;
+        return command;
     }
 
 }
