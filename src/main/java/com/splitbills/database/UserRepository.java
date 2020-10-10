b750603ea81146bb7a147c1d8bf7e1fb5dd95143
Ivelina <ivelinavul@gmail.com>
2020-10-10 09:04:50 +0300

    Remove not needed access modifiers

--- src/main/java/com/splitbills/database/UserRepository.java
+++ src/main/java/com/splitbills/database/UserRepository.java
@@ -3,7 +3,10 @@ package com.splitbills.database;
 import com.splitbills.database.models.User;
 
 public interface UserRepository {
-    public void add(User user) throws UserAlreadyExistsException;
-    public User get(String username);
-    public boolean contains(User user);
+
+    void add(User user) throws UserAlreadyExistsException;
+
+    User get(String username);
+
+    boolean contains(User user);
 }
