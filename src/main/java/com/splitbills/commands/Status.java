45fddf2bc87f765d8e2ea61ae31dd8b5fdeeb9f1
Ivelina <ivelinavul@gmail.com>
2020-10-27 17:08:51 +0200

    Add new status for not matching password and username

--- src/main/java/com/splitbills/commands/Status.java
+++ src/main/java/com/splitbills/commands/Status.java
@@ -4,10 +4,10 @@ public enum Status {
     OK("ok"),
     ALREADY_EXISTS("already-exists"),
     INVALID_ARGUMENTS("invalid-arguments"),
+    NOT_MATCHING_ARGUMENTS("not-matching"),
     SERVER_ERROR("server-error"),
     NOT_LOGGED_IN("not-logged-in"),
     ALREADY_LOGGED_IN("already-logged-in"),
-    NOT_EXISTING("not-existing"),
     NOT_REGISTERED("not-registered"),
     INVALID_COMMAND("invalid-command");
     private String message;
