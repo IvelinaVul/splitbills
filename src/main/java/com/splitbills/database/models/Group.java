bd4bde9fbfc0b717b28dc043eb62a2f0036586c2
Ivelina <ivelinavul@gmail.com>
2020-10-27 11:04:27 +0200

    Fix group repository

--- src/main/java/com/splitbills/database/models/Group.java
+++ src/main/java/com/splitbills/database/models/Group.java
@@ -3,7 +3,6 @@ package com.splitbills.database.models;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "group_table")
@@ -13,19 +12,20 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
-    @ManyToMany(targetEntity = User.class,cascade = {CascadeType.MERGE})
+    @ManyToMany(targetEntity = User.class, cascade = {CascadeType.MERGE}, fetch = FetchType.LAZY)
     @JoinTable(name = "user_group",
             joinColumns = @JoinColumn(name = "group_id"),
             inverseJoinColumns = @JoinColumn(name = "username"))
     private List<User> users;
-
-    @OneToMany(mappedBy = "groupId", cascade = CascadeType.ALL, targetEntity = Debt.class)
+    @OneToMany(mappedBy = "groupId", cascade = CascadeType.ALL, targetEntity = Debt.class, fetch = FetchType.LAZY)
     private List<Debt> debts;
-    @OneToMany(mappedBy = "groupId", cascade = CascadeType.ALL, targetEntity = PayRecord.class)
+    @OneToMany(mappedBy = "groupId", cascade = CascadeType.ALL, targetEntity = PayRecord.class, fetch = FetchType.LAZY)
     private List<PayRecord> history;
 
     public Group() {
-
+        users = new ArrayList<>();
+        debts = new ArrayList<>();
+        history = new ArrayList<>();
     }
 
     public Group(String name, List<User> users) {
@@ -39,8 +39,27 @@ public class Group {
         return id;
     }
 
-
     public String getName() {
         return name;
+    }
+
+    public List<Debt> getDebts() {
+        return debts;
+    }
+
+    public void addDebt(Debt debt) {
+        debts.add(debt);
+    }
+
+    public List<PayRecord> getHistory() {
+        return history;
+    }
+
+    public void addPayRecord(PayRecord payRecord) {
+        history.add(payRecord);
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 }
