83cca7d7420e95334d3fef529804e7040e55f7e9
Ivelina <ivelinavul@gmail.com>
2020-10-27 16:34:48 +0200

    Add menu

---
+++ src/main/java/com/splitbills/client/UserStorage.java
@@ -0,0 +1,23 @@
+package com.splitbills.client;
+
+public class UserStorage {
+
+    private String authenticationToken;
+    private boolean loggedIn = false;
+
+    public String getAuthenticationToken() {
+        return authenticationToken;
+    }
+
+    public void setAuthenticationToken(String authenticationToken) {
+        this.authenticationToken = authenticationToken;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+}
---
+++ src/main/java/com/splitbills/client/io/SimpleReader.java
@@ -0,0 +1,23 @@
+package com.splitbills.client.io;
+
+import java.util.Scanner;
+
+public class SimpleReader implements Reader {
+
+    private Scanner scanner;
+    private String delimiter;
+
+    public SimpleReader() {
+        delimiter = " ";
+        scanner = new Scanner(System.in);
+    }
+
+    public String readNext() {
+        return scanner.next();
+    }
+
+    public String[] readAllTillNextLine() {
+        String line = scanner.nextLine();
+        return line.split(delimiter);
+    }
+}
